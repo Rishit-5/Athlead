9c08dbe478f185cc1e44211a478848f683a3197e
KhushMakadia4 <kmakadia4@gmail.com>
2021-03-26 00:14:20 -0500

    big

--- javascript1.js
+++ javascript1.js
@@ -18,6 +18,10 @@ var imageVs = [];
 var imgName, imgUrl;
 var reader;
 var loggedIN = false;
+var allPosts = [];
+var totalPosts = [];
+var postNames = [];
+
 document.getElementById("enterBtn").onclick = function () {
     nameV = document.getElementById("namebox").value;
     emailV = document.getElementById("emailbox").value;
@@ -38,6 +42,14 @@ document.getElementById("enterBtn").onclick = function () {
                             firebase.database().ref("Users/" + nameV + "/Email").on('value', function (snapshot) {
                                 emailV = snapshot.val();
                                 loggedIN = true;
+                                firebase.database().ref('Users').once('value', function (allRecords) {
+                                    allRecords.forEach(
+                                        function (CurrentRecord) {
+                                            var name = CurrentRecord.val().Name;
+                                            names.push(name);
+                                        }
+                                    )
+                                });
                             });
 
                             document.getElementById("signinScreen").hidden = true;
@@ -48,6 +60,7 @@ document.getElementById("enterBtn").onclick = function () {
                             alert("incorrect")
                         }
                     } else {
+                        alert("We can't seem to find your account, would you like to sign up?")
                         // firebase.database().ref("Users/" + nameV).set({
                         //     Name: nameV,
                         //     Email: emailV,
@@ -85,63 +98,108 @@ document.getElementById("signupenterBtn").onclick = function() {
 
     if (!(nameV == "") && !(emailV == "") && !(passWV == "")) {
         var alreadyExists = false;
-        var finished = false;
 
         // do {
-            firebase.database().ref("Users").once('value', function (users) {
-                if (users.exists()) {
-                    users.forEach(function (user) {
-                        if (user.val().Name === nameV) {
-                            if (user.val().Email === emailV && user.val().Password === passWV) {
-                                alert("You already have an account, welcome back");
-                                alreadyExists = true;
-                                loggedIN = true;
-                                document.getElementById("signupScreen").hidden = true;
-                                document.getElementById("app").hidden = false;
-                                hideMainDivs();
-                                document.getElementById("homePage").hidden = true;
-                            } else {
-                                alert("Username already taken, please try again")
-                                alreadyExists = true;
-                            }
+        firebase.database().ref("Users").once('value', function (users) {
+            if (users.exists()) {
+                users.forEach(function (user) {
+                    if (user.val().Name === nameV) {
+                        if (user.val().Email === emailV && user.val().Password === passWV) {
+                            alert("You already have an account, welcome back");
+                            alreadyExists = true;
+                            loggedIN = true;
+                            document.getElementById("signupScreen").hidden = true;
+                            document.getElementById("app").hidden = false;
+                            hideMainDivs();
+                            document.getElementById("homePage").hidden = true;
+                        } else {
+                            alert("Username already taken, please try again")
+                            alreadyExists = true;
                         }
-                    });
-                }
-                if (!alreadyExists) {
-                    firebase.database().ref("Users/" + nameV).set({
-                        Name: nameV,
-                        Email: emailV,
-                        Password: passWV,
-                        Followers: 0,
+                    }
+                });
+            }
+            if (!alreadyExists) {
+                firebase.database().ref("Users/" + nameV).set({
+                    Name: nameV,
+                    Email: emailV,
+                    Password: passWV,
+                    Followers: 0,
 
-                    });
-                    loggedIN == true;
+                });
+                loggedIN == true;
 
-                    alert("Welcome aboard")
-                    document.getElementById("signupScreen").hidden = true;
-                    document.getElementById("app").hidden = false;
-                    hideMainDivs();
-                    document.getElementById("homePage").hidden = true;
-                }
-                finished = true;
-            });
-        // }while (!finished);
-
-
+                alert("Welcome aboard")
+                document.getElementById("signupScreen").hidden = true;
+                document.getElementById("app").hidden = false;
+                hideMainDivs();
+                document.getElementById("homePage").hidden = true;
+            }
+        });
 
     }
 }
+
 
 document.getElementById("homeBtn").onclick = function () {
     hideMainDivs();
     document.getElementById("homePage").hidden = false;
 }
+let names = [];
 
 document.getElementById("searchBtn").onclick = function () {
     hideMainDivs();
     document.getElementById("searchPage").hidden = false;
-}
+    // names = getNames(names)
+    for (let i = 0; i < totalPosts.length; i++) {
+        document.getElementById('searchPage').removeChild(totalPosts[i])
+    }
+    totalPosts = [];
+    allPosts = [];
+    postNames = [];
+    for (let i = 0; i < names.length; i++) {
+        if (names[i] === nameV) {
 
+        }
+        else {
+            firebase.database().ref('Users/' + names[i] + "/Posts").once('value', function (allRecords) {
+                allRecords.forEach(
+                    function (CurrentRecord) {
+                        var link = CurrentRecord.val().Link;
+                        var img = document.createElement('img');
+                        var postName = link.substring(link.indexOf("%2F")+3, link.indexOf(".png"))
+                        postName = postName.replaceAll("%20"," ")
+                        postName = postName.toLowerCase();
+                        postNames.push(postName)
+
+                        img.src = link
+                        document.getElementById('searchPage').appendChild(img);
+                        totalPosts.push(img);
+                        var type = CurrentRecord.val().Type;
+                        var arrayA = [link, type, names[i]];
+                        allPosts.push(arrayA);
+                    }
+                )
+            })
+        }
+    }
+}
+function updateSearch(){
+    for (let i = 0; i < totalPosts.length; i++) {
+        document.getElementById('searchPage').removeChild(totalPosts[i])
+    }
+    totalPosts = [];
+    var searchText = document.getElementById("searchbar").value;
+    for (let i = 0; i < postNames.length; i++) {
+        if (postNames[i].includes(searchText.toLowerCase())){
+            var img = document.createElement('img');
+            img.src = allPosts[i][0]
+            document.getElementById('searchPage').appendChild(img);
+            totalPosts.push(img);
+        }
+    }
+    // alert(document.getElementById("searchbar").value)
+}
 
 document.getElementById("myprofileBtn").onclick = function () {
     hideMainDivs();
@@ -149,13 +207,17 @@ document.getElementById("myprofileBtn").onclick = function () {
     document.getElementById("myprofile").hidden = false;
     document.getElementById("postingPage").hidden = true;
     var pfpsrc;
-    firebase.database().ref("Users/" + nameV + "/PFP/" + "Link").on('value', function (snapshot) {
+    firebase.database().ref("Users/" + nameV + "/PFP/" + "Link").once('value', function (snapshot) {
         pfpsrc = snapshot.val();
     });
-    document.getElementById("pfp").src = pfpsrc;
+    if (pfpsrc != null) {
+        document.getElementById("pfp").src = pfpsrc;
+    }
+
     for (let i = 0; i < yourPosts.length; i++) {
         document.getElementById('myprofilePage').removeChild(yourPosts[i])
     }
+    yourPosts = [];
     firebase.database().ref("Users/"+nameV+"/Posts").once('value', function (snapshot) {
         snapshot.forEach(function (child) {
             var str = "Users/"+nameV+"/Posts/"+child.key + "/Link";
